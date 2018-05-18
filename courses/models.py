abf9f42501118619725b244f380016db2e45d717
shenyoujian <shenyoujian2.163.com>
2018-05-18 16:58:12 +0800

    4-6 设计完成课程app中四张数据表：课程，章节，视频，资源

---
+++ courses/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+
+# 课程信息表
+class Course(models.Model):
+    DEGREE_CHOICES = (
+        ("cj", u"初级"),
+        ("zj", u"中级"),
+        ("gj", u"高级"),
+    )
+    name = models.CharField(max_length=50, verbose_name=u"课程名")
+    desc = models.CharField(max_length=300, verbose_name=u"课程描述")
+    # TextField允许我们不输入长度。可以输入到无限大。暂时定义为TextFiled，之后更新为富文本
+    detail = models.TextField(verbose_name=u"课程详情")
+    degree = models.CharField(max_length=2, choices=DEGREE_CHOICES)
+    # 使用分钟做后台记录(存储最小单位)前台转换
+    learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
+    # 保存学习的人数：点击开始学习才算
+    students = models.IntegerField(default=0, verbose_name=u"学习人数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
+    image = models.ImageField(
+        upload_to="courses/%Y/%m",
+        max_length=100,
+        verbose_name=u"课程封面图"
+    )
+    # 保存点击量，点击页面就算
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    add_time = models = models.DateTimeField(default=datetime.now, verbose_name=u"课程添加的时间")
+
+    class Meta:
+        verbose_name = u"课程"
+        verbose_name_plural = verbose_name
+
+
+# 章节
+class Lesson(models.Model):
+    # 因为一个课程对应很多章节。所以在章节表中将课程设置为外键。
+    # 作为一个字段来让我们可以知道这个章节对应那个课程
+    course = models.ForeignKey(Course, verbose_name=u"课程")
+    name = models.CharField(max_length=100, verbose_name=u"章节名")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"章节添加的时间")
+
+    class Meta:
+        verbose_name = u"章节"
+        verbose_name_plural = verbose_name
+
+
+# 每章视频
+class Video(models.Model):
+    # 因为一个章节对应很多视频。所以在视频表中将章节设置为外键。
+    # 作为一个字段来存储让我们可以知道这个视频对应哪个章节.
+    lesson = models.ForeignKey(Lesson, verbose_name=u"章节")
+    name = models.CharField(max_length=100, verbose_name=u"视频名")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"该视频添加的时间")
+
+    class Meta:
+        verbose_name = u"视频"
+        verbose_name_plural = verbose_name
+
+
+# 课程资源
+class CourseResource(models.Model):
+    # 因为一个课程对应很多资源。所以在课程资源表中将课程设置为外键。
+    # 作为一个字段来让我们可以知道这个资源对应那个课程
+    course = models.ForeignKey(Course, verbose_name=u"课程")
+    name = models.CharField(max_length=100, verbose_name=u"名称")
+    # 这里定义成文件类型的field，后台管理系统中会直接有上传的按钮。
+    # FileField也是一个字符串类型，要指定最大长度。
+    download = models.FileField(
+        upload_to="course/resource/%Y/%m",
+        verbose_name=u"资源文件",
+        max_length=100,
+    )
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"该资源添加的时间")
+
+    class Meta:
+        verbose_name = u"课程资源"
+        verbose_name_plural = verbose_name
+
+
+
+
+
+
+
+
+
+
+
+
