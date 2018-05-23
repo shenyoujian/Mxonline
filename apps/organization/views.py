53d7a39d0b697fd4268356e71ec5648194458ceb
shenyoujian <shenyoujian2.163.com>
2018-05-24 01:36:29 +0800

    7-3, 4 & 5:完成列表数据展示列表分页功能：使用pure_pagination

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -14,9 +14,21 @@ class OrgView(View):
         # 取出所有的城市
         all_citys = CityDict.objects.all()
 
+        # 对课程机构进行分页
+        # 尝试获取前台get请求传递过来的page参数
+        # 如果不合法的配置参数默认返回第一页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        # 这里指从allorg中取五个出来，每页显示5个
+        p = Paginator(all_orgs, 5, request=request)
+        orgs = p.page(page)
+
         # 总共有多少家机构使用count进行统计
-        # org_nums = all_orgs.count()
+        org_nums = all_orgs.count()
         return render(request, "org-list.html",{
-            "all_orgs":all_orgs,
+            "all_orgs": orgs,
             "all_citys": all_citys,
+            "org_nums": org_nums,
         })
