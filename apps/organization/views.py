5befeb85f7d7e337164eed991c875b7e767ea81a
shenyoujian <shenyoujian2.163.com>
2018-05-24 01:08:49 +0800

    完成后台数据添加，列表页数据展示。对应7-3&7-4

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -8,4 +8,15 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 # 处理课程机构列表的view
 class OrgView(View):
     def get(self, request):
-        return render(request, "org-list.html")
+        # 查找到所有的课程机构
+        all_orgs = CourseOrg.objects.all()
+
+        # 取出所有的城市
+        all_citys = CityDict.objects.all()
+
+        # 总共有多少家机构使用count进行统计
+        # org_nums = all_orgs.count()
+        return render(request, "org-list.html",{
+            "all_orgs":all_orgs,
+            "all_citys": all_citys,
+        })
